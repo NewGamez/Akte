c2eaf99142826e22edf63e400ddfc06c08483043
Bl4ck_Schnitzel <104429747+NewGamez@users.noreply.github.com>
2025-07-29 17:41:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -33,28 +33,28 @@ Narco City Police Department
 ${get("officer")}
 
 | Tatort: | ${get("tatort")}
-|Zeitraum: | ${get("zeitraum")}
+| Zeitraum: | ${get("zeitraum")}
 
-|Beschuldigte: | ${get("beschuldigte")}
-|Geschädigte: | ${get("geschaedigte")}
+| Beschuldigte: | ${get("beschuldigte")}
+| Geschädigte: | ${get("geschaedigte")}
 
-|Sachverhalt: |
+| Sachverhalt: |
 ${get("sachverhalt")}
 
-|Einheiten/Zeugen: | ${get("einheiten")}
+| Einheiten/Zeugen: | ${get("einheiten")}
 
-|Gegenstände: |
+| Gegenstände: |
 ${get("gegenstaende")}
 |Abgenommen von: | ${get("abgenommenVon")}
 
-|Bemerkungen: |
+| Bemerkungen: |
 Die Rechte wurden dem Beschuldigten durch ${get("rechteVon")} im Beisein von ${get("rechteBeisein")} verlesen und verstanden.
 Dieser ${checked("checkboxRechtsbeistand_s") ? "bestand" : "verzichtete"} auf einen Rechtsbeistand.
 Der TV ${checked("checkboxMedizin_s") ? "bestand" : "verzichtete"} auf medizinische Versorgung.
 ${get("bussgeld") ? `Das Bußgeld ist bis zum ${formatBussgeldDatum(get("bussgeld"))} [+7 Tage] zu bezahlen.` : ""}
 ${get("bemerkung")}
 
-|Gezeichnet von: |
+| Gezeichnet von: |
 ${get("zeichner")} (${get("zeichnerInfo")})
 
 ${get("strafen")}
@@ -73,26 +73,26 @@ function generateSchnellakte() {
 Narco City Police Department
 ${get("s_officer")}
 
-|Tatort: | ${get("s_tatort")}
-|Tatzeit: | ${get("s_zeitraum")}
+| Tatort: | ${get("s_tatort")}
+| Tatzeit: | ${get("s_zeitraum")}
 
-|Beschuldigte: | ${get("s_beschuldigte")}
-|Geschädigte: | ${get("s_geschaedigte")}
+| Beschuldigte: | ${get("s_beschuldigte")}
+| Geschädigte: | ${get("s_geschaedigte")}
 
-|Einheiten/Zeugen: | ${get("s_einheiten")}
+| Einheiten/Zeugen: | ${get("s_einheiten")}
 
-|Gegenstände: |
+| Gegenstände: |
 ${get("s_gegenstaende")}
 |Abgenommen von: ${get("s_abgenommenVon")}
 
-|Bemerkungen: |
+| Bemerkungen: |
 Die Rechte wurden dem Beschuldigten durch ${get("s_rechteVon")} im Beisein von ${get("s_rechteBeisein")} verlesen und verstanden.
 Dieser ${checked("checkboxRechtsbeistand_sn") ? "bestand" : "verzichtete"} auf einen Rechtsbeistand.
 Der TV ${checked("checkboxMedizin_sn") ? "bestand" : "verzichtete"} auf medizinische Versorgung.
 ${get("s_bussgeld") ? `Das Bußgeld ist bis zum ${formatBussgeldDatum(get("s_bussgeld"))} [+7 Tage] zu bezahlen.` : ""}
 ${get("s_bemerkung")}
 
-|Gezeichnet von: |
+| Gezeichnet von: |
 ${get("s_gezeichnet")} (${get("s_gezeichnetOfficer")})
 `.trim();
 
@@ -109,23 +109,23 @@ function generateKollektivakte() {
 Narco City Police Department
 ${get("k_officer")}
 
-|Genehmigt von: | ${get("k_genehmigtVon")} um ${get("k_uhrzeit")}
-|Tatort: | ${get("k_tatort")}
-|Tatzeitraum: | ${get("k_zeitraum")}
+| Genehmigt von: | ${get("k_genehmigtVon")} um ${get("k_uhrzeit")}
+| Tatort: | ${get("k_tatort")}
+| Tatzeitraum: | ${get("k_zeitraum")}
 
-|Beschuldigte: | ${get("k_beschuldigte")}
-|Geschädigte: | ${get("k_geschaedigte")}
+| Beschuldigte: | ${get("k_beschuldigte")}
+| Geschädigte: | ${get("k_geschaedigte")}
 
-|Sachverhalt:
+| Sachverhalt:
 ${get("k_sachverhalt")}
 
-|Einheiten/Zeugen: ${get("k_einheiten")}
+| Einheiten/Zeugen: ${get("k_einheiten")}
 
-|Gegenstände: |
+| Gegenstände: |
 ${get("k_gegenstaende")}
-|Abgenommen von: | ${get("k_abgenommenVon")}
+| Abgenommen von: | ${get("k_abgenommenVon")}
 
-|Bemerkungen: |
+| Bemerkungen: |
 Die Rechte wurden dem Beschuldigten durch ${get("k_rechteVon")} im Beisein von ${get("k_rechteBeisein")} verlesen und verstanden.
 Dieser ${checked("checkboxRechtsbeistand_k") ? "bestand" : "verzichtete"} auf einen Rechtsbeistand.
 Der TV ${checked("checkboxMedizin_k") ? "bestand" : "verzichtete"} auf medizinische Versorgung.
